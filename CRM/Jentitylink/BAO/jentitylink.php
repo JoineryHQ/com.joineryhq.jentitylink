87bfc0b35714f59537656f51dcdf277365476556
Allen Shaw <allen@JoineryHQ.com>
2023-08-18 17:06:55 -0500

    Add jentitylink entity

---
+++ CRM/Jentitylink/BAO/jentitylink.php
@@ -0,0 +1,30 @@
+<?php
+// phpcs:disable
+use CRM_Jentitylink_ExtensionUtil as E;
+// phpcs:enable
+
+class CRM_Jentitylink_BAO_jentitylink extends CRM_Jentitylink_DAO_jentitylink {
+
+  /**
+   * Create a new Jentitylink based on array-data
+   *
+   * @param array $params key-value pairs
+   * @return CRM_Jentitylink_DAO_jentitylink|NULL
+   */
+  /*
+  public static function create($params) {
+    $className = 'CRM_Jentitylink_DAO_jentitylink';
+    $entityName = 'Jentitylink';
+    $hook = empty($params['id']) ? 'create' : 'edit';
+
+    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+    $instance = new $className();
+    $instance->copyValues($params);
+    $instance->save();
+    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
+
+    return $instance;
+  }
+  */
+
+}
---
+++ Civi/Api4/Jentitylink.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * Jentitylink entity.
+ *
+ * Provided by the Entity Navigation extension.
+ *
+ * @package Civi\Api4
+ */
+class Jentitylink extends Generic\DAOEntity {
+
+}
