c5d7f5eaf79a7d6bb71996a9b63d03327fb3f347
Allen Shaw <allen@JoineryHQ.com>
2023-08-18 17:10:42 -0500

    Add jenittylinkOp entity

---
+++ Civi/Api4/JentitylinkOp.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * JentitylinkOp entity.
+ *
+ * Provided by the Entity Navigation extension.
+ *
+ * @package Civi\Api4
+ */
+class JentitylinkOp extends Generic\DAOEntity {
+
+}
